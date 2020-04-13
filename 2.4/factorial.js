21d8a0fefc29e8f640907adfbffef9016183ed51
ChlorUpload <chlor.upload@gmail.com>
2020-04-13 18:10:42 +0900

    Autoparse factorial.js using project-manager

---
+++ 2.4/factorial.js
@@ -0,0 +1,9 @@
+//factorial.js
+function fact(n) {
+    if (n <= 1)
+        return n;
+    return n * fact(n-1);
+}
+for (var i = 1; i < 10; i++) {
+    console.log(i + "! = " + fact(i));
+}
--- project-manager.py
+++ project-manager.py
@@ -4,10 +4,30 @@ from os.path import isfile, join
 path = './'
 folders = [f for f in listdir(path) if not isfile(join(path, f))]
 
-print(folders)
+# print(folders)
 
 for elem in folders:
     if elem == '.git':
         continue
-    newname = elem.split(' ')[0]
-    rename(join(path, elem), join(path, newname))
+    # read folder
+    try:
+        readme = open(join(elem, path, 'readme.md'), 'rt', encoding='utf8')
+    except:
+        continue
+
+    lines = readme.readlines()
+
+    codefence = False
+    for i in range(len(lines)):
+        if '```javascript' in lines[i]:
+            codefence = True
+            i+=1
+            jsfile = open(join(elem, (lines[i])[2:-1]), 'wt', encoding='utf8')
+            continue
+        if '```' in lines[i] and '```javascript' not in lines[i]:
+            codefence = False
+            jsfile.close()
+            continue
+    
+        if codefence:
+            jsfile.write(lines[i])
